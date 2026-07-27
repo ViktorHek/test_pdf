842a1e743e15468a8f87d6d9edbba4dc404f5ded
viktor-nord <viktor.karlsson@edunord.se>
2026-07-27 13:19:15 +0200

    adding img

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 
 function App() {
   return (
-    <div className="main">
-      <p>hello</p>
+    <div className="container">
+      <img src="./5E_CharacterSheet_Fillable_page-0001.jpg" alt="gg" />
+      <div className="inputs-container">
+        <span className="name">Axel Havoc</span>
+      </div>
+      <input type="text" />
     </div>
   );
 }
