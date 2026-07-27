f3c515f968ad30187c03c90f9125455b7c159eca
viktor-nord <viktor.karlsson@edunord.se>
2026-07-27 13:41:14 +0200

    gg

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 function App() {
   return (
     <div className="container">
-      <img src="./5E_CharacterSheet_Fillable_page-0001.jpg" alt="gg" />
       <div className="inputs-container">
         <span className="name">Axel Havoc</span>
+        <input type="text" />
       </div>
-      <input type="text" />
+      <img src="./5E_CharacterSheet_Fillable_page-0001.jpg" alt="gg" width="850px" height="1100px" />
     </div>
   );
 }
